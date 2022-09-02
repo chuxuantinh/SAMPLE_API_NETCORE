f64f2facf6e8ffef4725a268c8b969e02468f966
Chu Xuân Tình <chuxuantinhcs11@gmail.com>
2022-09-03 01:33:17 +0700

    Create C_User.cs

---
+++ SAMPLE_API_NETCORE/Models/C_User.cs
@@ -0,0 +1,21 @@
+﻿namespace SAMPLE_API_NETCORE.Models
+{
+  
+    public class C_User
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? PassWord { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? Department { get; set; }
+        public string? Position { get; set; }
+        public int? RoleId { get; set; }
+        public int? Status { get; set; }
+        public bool? IsDeleted { get; set; }
+        public string? GroupId { get; set; }
+        public string? UserType { get; set; }
+    }
+}
