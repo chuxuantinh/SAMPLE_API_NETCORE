5ff24776fd70c3efc8824f15a762ba11807c264f
Chu Xuân Tình <chuxuantinhcs11@gmail.com>
2022-09-03 01:33:38 +0700

    Update Program.cs

--- SAMPLE_API_NETCORE/Program.cs
+++ SAMPLE_API_NETCORE/Program.cs
@@ -1,11 +1,23 @@
+using SAMPLE_API_NETCORE.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddEntityFrameworkSqlServer().AddDbContext<LABHUB2022Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Connect_st"));
+});
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
